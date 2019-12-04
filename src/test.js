3773661c898221030a912a708bd88acd64f54093
chisasy <n.chisato0512@gmail.com>
2019-12-04 23:05:11 +0900

    color and NG

--- src/test.js
+++ src/test.js
@@ -7,16 +7,40 @@ function Calcu(a,b,c){
         c = document.getElementsByClassName("symbol")[i].innerHTML;
     
         if(c === '+'){
-            document.getElementsByClassName("result")[i].value = a+b;
+            const check = document.getElementsByClassName("result")[i].value = a+b;
+            if(check > 100){
+                document.getElementsByClassName("check")[i].innerHTML = 'OK';
+            }else{
+                document.getElementsByClassName("check")[i].innerHTML = 'NG';
+                document.getElementsByClassName("check")[i].style.backgroundColor = "red";
+            }
         }
         if(c === '−'){
-            document.getElementsByClassName("result")[i].value = a-b;
+            const check = document.getElementsByClassName("result")[i].value = a-b;
+            if(check > 100){
+                document.getElementsByClassName("check")[i].innerHTML = 'OK';
+            }else{
+                document.getElementsByClassName("check")[i].innerHTML = 'NG';
+                document.getElementsByClassName("check")[i].style.backgroundColor = "red";
+            }
         }
         if(c === '×'){
-            document.getElementsByClassName("result")[i].value = a*b;
+            const check = document.getElementsByClassName("result")[i].value = a*b;
+            if(check > 100){
+                document.getElementsByClassName("check")[i].innerHTML = 'OK';
+            }else{
+                document.getElementsByClassName("check")[i].innerHTML = 'NG';
+                document.getElementsByClassName("check")[i].style.backgroundColor = "red";
+            }
         }
         if(c === '÷'){
-            document.getElementsByClassName("result")[i].value = a/b;
+            const check = document.getElementsByClassName("result")[i].value = a/b;
+            if(check > 100){
+                document.getElementsByClassName("check")[i].innerHTML = 'OK';
+            }else{
+                document.getElementsByClassName("check")[i].innerHTML = 'NG';
+                document.getElementsByClassName("check")[i].style.backgroundColor = "red";
+            }
         }
     }
 }
