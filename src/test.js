1a17d544d178e6472c00e099dbe7db190435eca9
chisasy <n.chisato0512@gmail.com>
2019-12-04 22:30:21 +0900

    calucmethod

--- src/test.js
+++ src/test.js
@@ -1,24 +1,22 @@
 
-function Addition(a,b){
-    a = parseFloat(document.getElementsByClassName("first")[0].value);
-    b = parseFloat(document.getElementsByClassName("second")[0].value);        
-    document.getElementsByClassName("result")[0].value = a+b;
-}
+function Calcu(a,b,c){
 
-function Subtraction(a,b){
-    a = parseFloat(document.getElementsByClassName("first")[1].value);
-    b = parseFloat(document.getElementsByClassName("second")[1].value);        
-    document.getElementsByClassName("result")[1].value = a-b;
-}
-
-function Multiplication(a,b){
-    a = parseFloat(document.getElementsByClassName("first")[2].value);
-    b = parseFloat(document.getElementsByClassName("second")[2].value);        
-    document.getElementsByClassName("result")[2].value = a*b;
-}
-
-function Division(a,b){
-    a = parseFloat(document.getElementsByClassName("first")[3].value);
-    b = parseFloat(document.getElementsByClassName("second")[3].value);        
-    document.getElementsByClassName("result")[3].value = a/b;
+    for (let i = 0; i < 4; i++) {
+        a = parseFloat(document.getElementsByClassName("first")[i].value);
+        b = parseFloat(document.getElementsByClassName("second")[i].value); 
+        c = document.getElementsByClassName("symbol")[i].innerHTML;
+    
+        if(c === '+'){
+            document.getElementsByClassName("result")[i].value = a+b;
+        }
+        if(c === '−'){
+            document.getElementsByClassName("result")[i].value = a-b;
+        }
+        if(c === '×'){
+            document.getElementsByClassName("result")[i].value = a*b;
+        }
+        if(c === '÷'){
+            document.getElementsByClassName("result")[i].value = a/b;
+        }
+    }
 }
